d459a6011f30174693b29d8a91aabf9010fa2513
Thomas <thomasweems3@gmail.com>
2019-04-12 16:52:41 -0500

    test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -22,6 +22,10 @@
       <label for="email">Your email</label>
       <input type="email" name="email" v-model="formData.email" />
     </div>
+    <div>
+      <label for="phone">Your email</label>
+      <input type="text" name="phone" v-model="formData.phone" />
+    </div>
   </div>
 
   <div class="message-wrapper">
