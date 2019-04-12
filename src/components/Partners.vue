05ec431181e654ac7449e1581a0ebb75bf32d32e
Thomas <thomasweems3@gmail.com>
2019-04-12 11:19:18 -0500

    added partner images back after resetting

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -3,9 +3,15 @@
     <h2 class="partners-header">Partnerships</h2>
     <div class="partners-background">
       <div class="partners-content">
-        <Partner :icon="'dns'" :title="'Partner Title'" />
-        <Partner :icon="'dns'" :title="'Partner Title'" />
-        <Partner :icon="'dns'" :title="'Partner Title'" />
+        <img src="../../static/assets/cisco_logo.png" alt="cisco" class="partner-img">
+        <img src="../../static/assets/vmware_logo.png" alt="vmware" class="partner-img">
+        <img src="../../static/assets/microsoft_logo.png" alt="microsoft" class="partner-img">
+        <img src="../../static/assets/dellemc_logo.jpg" alt="dellemc" class="partner-img">
+        <img src="../../static/assets/aws_logo.png" alt="aws" class="partner-img">
+        <img src="../../static/assets/veeam_logo.jpg" alt="veeam" class="partner-img">
+        <img src="../../static/assets/acronis_logo.png" alt="acronis" class="partner-wide-img">
+        <img src="../../static/assets/ingram_logo.png" alt="ingram" class="partner-wide-img">
+        <img src="../../static/assets/watchguard_logo.png" alt="watchguard" class="partner-wide-img">
       </div>
     </div>
   </div>
@@ -15,9 +21,6 @@
 import Partner from '@/components/Partner.vue'
 export default {
     name: 'Partners',
-    components: {
-      Partner
-    }
 }
 </script>
 
@@ -43,8 +46,31 @@ export default {
     width: 100%;
     max-width: 1120px;
     display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
+    flex-wrap: wrap;
     padding: 50px 0;
     margin: 0 auto;
+  }
+  .partner-img {
+    width: 200px;
+    height: 150px;
+    margin: 20px;
+    align-self: center;
+    border-radius: 15px;
+    background-color: white;
+  }
+  .partner-wide-img {
+    width: 400px;
+    height: 100px;
+    margin: 20px;
+    align-self: center;
+    background-color: white;
+    border-radius: 15px;
+  }
+  @media screen and (max-width: 550px) {
+    .partner-wide-img {
+      width: 300px;
+      height: 70px;
+    }
   }
 </style>
