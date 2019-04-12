83769955157ec4d553e84e1060d55a3cbd5306c7
Thomas <thomasweems3@gmail.com>
2019-04-11 22:31:23 -0500

    added netlify attributes to form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,23 +1,64 @@
 <template>
-    <form action="">
+    <form @submit.prevent="validateSubmit" name="Free Assessment Form" data-netlify="true" netlify-honeypot="bot-field">
       <span class="slogan">{{ slogan }}</span>
-      <input type="text" name="Business Name" placeholder="Business Name">
-      <input type="text" name="First Name" placeholder="First Name">
-      <input type="text" name="Last Name" placeholder="Last Name">
-      <input type="email" name="Email" placeholder="Email">
-      <input type="text" name="Phone Number" placeholder="Phone Number">
-      <button type="button" class="form-btn">Submit</button>
+      <p class="hidden">
+        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+      </p>
+      <input @input="ev => business_name = ev.target.value" type="text" name="Business Name" placeholder="Business Name" :class="{'input-error': error}" @change="error = false">
+      <input @input="ev => first_name = ev.target.value" type="text" name="First Name" placeholder="First Name" :class="{'input-error': error}" @change="error = false">
+      <input @input="ev => last_name = ev.target.value" type="text" name="Last Name" placeholder="Last Name" :class="{'input-error': error}" @change="error = false">
+      <input @input="ev => phone = ev.target.value" type="email" name="Email" placeholder="Email" :class="{'input-error': error}" @change="error = false">
+      <input @input="ev => email = ev.target.value" type="text" name="Phone Number" placeholder="Phone Number" :class="{'input-error': error}" @change="error = false">
+      <button type="submit" class="form-btn">Submit</button>
     </form>
 </template>
 
 <script>
 export default {
     name: 'Form',
-    props: ['slogan']
+    props: ['slogan'],
+    data() {
+      return {
+        business_name: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        error: false
+      }
+    },
+    methods: {
+      validateSubmit() {
+        if(this.business_name == '' || this.business_name == null) {
+          this.error = true 
+          return;
+        }
+        if(this.first_name == '' || this.first_name == null) {
+          this.error = true
+          return;
+          }
+        if(this.last_name == '' || this.last_name == null) {
+          this.error = true
+          return;
+          }
+        if(this.email == '' || this.email == null) {
+          this.error = true
+          return;
+          }
+        if(this.phone == '' || this.phone == null) {
+          this.error = true
+          return;
+          }
+      }
+    }
 }
 </script>
 
 <style>
+  .hidden {
+    display: none;
+  }
+
   form {
     display: flex;
     flex-direction: column;
@@ -36,6 +77,10 @@ export default {
     margin-bottom: 10px;
     font-size: 1.1rem;
     width: 95%;
+  }
+
+  .input-error {
+    border: 1px solid red;
   }
 
   .form-btn {
