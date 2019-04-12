3441723f567698055e0c8bcabeb41ee1e8a086ae
Thomas <thomasweems3@gmail.com>
2019-04-12 17:01:19 -0500

    test

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,35 +7,34 @@
   data-netlify="true"
   data-netlify-honeypot="bot-field"
 >
+  <p class="slogan">{{slogan}}</p>
   <input type="hidden" name="form-name" value="contact" />
   <p hidden>
     <label>
       Don’t fill this out: <input name="bot-field" />
     </label>
   </p>
-  <div class="sender-info">
-    <div>
-      <label for="business_name" class="label" >Your name</label>
+    <div class="input-group">
+      <label for="business_name" class="label" >Business Name</label>
       <input type="text" name="business_name" v-model="formData.business_name" />
     </div>
-    <div>
-      <label for="first_name" class="label" >Your name</label>
+    <div class="input-group">
+      <label for="first_name" class="label" >First Name</label>
       <input type="text" name="first_name" v-model="formData.first_name" />
     </div>
-    <div>
-      <label for="last_name" class="label" >Your name</label>
+    <div class="input-group">
+      <label for="last_name" class="label" >Last Name</label>
       <input type="text" name="last_name" v-model="formData.last_name" />
     </div>
-    <div>
-      <label for="email">Your email</label>
+    <div class="input-group">
+      <label for="email">Email</label>
       <input type="email" name="email" v-model="formData.email" />
     </div>
-    <div>
-      <label for="phone">Your email</label>
+    <div class="input-group">
+      <label for="phone">Phone Number</label>
       <input type="text" name="phone" v-model="formData.phone" />
     </div>
-  </div>
-  <button type="submit">Submit form</button>
+  <button type="submit" class="form-btn">Submit</button>
 </form>
 </template>
 
@@ -77,7 +76,7 @@ export default {
     align-items: center;
     width: 500px;
     box-shadow: 0 2px 15px 8px rgba(0, 0, 0, 0.164); 
-    padding: 20px;
+    padding: 15px;
     border-radius: 5px;
     background: var(--white);
   }
@@ -111,10 +110,15 @@ export default {
     font-size: 1.5rem;
     font-weight: bold;
     margin-bottom: 10px;
+    margin-top: 0;
   }
 
   .input-error {
     border: 1px solid red;
+  }
+
+  .input-group {
+    width: 100%;
   }
 
   @media screen and (max-width: 767px) {
